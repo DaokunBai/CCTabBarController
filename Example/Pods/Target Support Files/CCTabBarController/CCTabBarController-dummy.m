2628c5c7a98eb7a69bfc2f851612757477576b11
Draveness <stark.draven@gmail.com>
2016-03-09 23:18:43 +0800

    Add gitignore

--- Example/Pods/Target Support Files/CCTabBarController/CCTabBarController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CCTabBarController : NSObject
-@end
-@implementation PodsDummy_CCTabBarController
-@end
--- Example/Pods/Target Support Files/Pods-CCTabBarController_Example/Pods-CCTabBarController_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CCTabBarController_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CCTabBarController_ExampleVersionString[];
-
