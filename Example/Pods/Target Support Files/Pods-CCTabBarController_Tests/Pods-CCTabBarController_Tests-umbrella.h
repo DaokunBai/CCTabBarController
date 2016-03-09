69620c60cd87a384d0b6fa26b63fb8270fffc46f
Draveness <stark.draven@gmail.com>
2016-03-09 23:19:07 +0800

    Add gitignore

--- Example/Pods/Target Support Files/Pods-CCTabBarController_Tests/Pods-CCTabBarController_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CCTabBarController_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CCTabBarController_TestsVersionString[];
-
