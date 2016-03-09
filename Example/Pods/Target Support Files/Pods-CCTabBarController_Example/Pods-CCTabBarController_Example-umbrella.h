217c89dc5c4414234d183fb04c74e23bc44aaf50
Draveness <stark.draven@gmail.com>
2016-03-09 23:17:30 +0800

    Initialize

---
+++ Example/Pods/Target Support Files/Pods-CCTabBarController_Example/Pods-CCTabBarController_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CCTabBarController_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CCTabBarController_ExampleVersionString[];
+
