6c224475539f695972d90d8cfe40cc303990a6c6
Draveness <stark.draven@gmail.com>
2016-03-10 00:42:06 +0800

    Add UIImage categoty color

---
+++ Pod/Classes/UIImage+Color.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Color.h
+//  Pods
+//
+//  Created by Draveness on 16/3/7.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Color)
+
++ (UIImage *)imageFromColor:(UIColor *)color;
+
+@end
