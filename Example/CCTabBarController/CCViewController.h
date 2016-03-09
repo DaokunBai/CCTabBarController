aa4f42df871d71c3e47870d2d06e8820041bbba7
Draveness <stark.draven@gmail.com>
2016-03-09 23:16:09 +0800

    Initial commit

---
+++ Example/CCTabBarController/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  CCTabBarController
+//
+//  Created by Draveness on 03/09/2016.
+//  Copyright (c) 2016 Draveness. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CCViewController : UIViewController
+
+@end
