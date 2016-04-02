d931ffcd2b4f77e2102117de28d15ec15f1049db
Draveness <stark.draven@gmail.com>
2016-04-02 13:18:20 +0800

    Build a page view controller

---
+++ Pod/Classes/CCScrollTabBar.h
@@ -0,0 +1,13 @@
+//
+//  CCScrollTabBar.h
+//  Pods
+//
+//  Created by Draveness on 16/4/2.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCScrollTabBar : UIView
+
+@end
---
+++ Pod/Classes/CCScrollTabBar.m
@@ -0,0 +1,13 @@
+//
+//  CCScrollTabBar.m
+//  Pods
+//
+//  Created by Draveness on 16/4/2.
+//
+//
+
+#import "CCScrollTabBar.h"
+
+@implementation CCScrollTabBar
+
+@end
